b7409103e5c0c0314d91faed48fb3f56eb5d21fa
John Paden <paden@ku.edu>
2026-08-26 23:21:37 -0500

    Commit yaml files, capellav1_sys

---
+++ capellav1_sys.m
@@ -0,0 +1,22 @@
+sys.name = 'Capella v1'
+sys.fc = 9.65e9;
+sys.B = 500e6;
+sys.inc_angle_deg = 45;
+sys.altitude = 525e3;
+sys.Pt = 600;
+sys.f_prf = 5000;
+sys.Tpd = 20e-6;
+sys.Gt_dB = 48;
+sys.Gr_dB = 48;
+sys.beta_x_deg = 0.39;
+sys.beta_y_deg = 1.65;
+% path_dir: root to processing source code
+sys.path_dir = 'C:\git\eecs800\';
+% temp_dir: store raw and processed files here
+sys.temp_dir = 'C:\Temp\eecs800_sar_capella\';
+
+addpath(fullfile(sys.path_dir));
+addpath(fullfile(sys.path_dir,'eecs800helper'));
+
+fn_capella_sys = fullfile(sys.temp_dir,'capella.yaml');
+yaml.dumpFile(fn_capella_sys, sys,'block');
